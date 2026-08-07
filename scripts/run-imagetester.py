c498d353c25891dbed26c5131cc7fcf496aeccd3
Pierre Meißner <pierre.meissner@strato.de>
2026-08-07 09:59:26 +0200

    Fix names for constants can not be resolved by inheritance

--- scripts/run-imagetester.py
+++ scripts/run-imagetester.py
@@ -6,15 +6,19 @@ from ImageBuilder.Util.ImageTester import ImageTester
 from ImageBuilder.Util.VmHelpers import VmCreationError
 from ImageBuilder.Util.EphemeralIpBlock import EphemeralIpBlock
 
-test_recipe_name = os.environ['RECIPE_NAME']
+def main():
+    test_recipe_name = os.environ['RECIPE_NAME']
 
-try:
-    with EphemeralIpBlock(test_recipe_name + '_ip') as ip_block:
-        tester = ImageTester()
-        tester.server_ip = ip_block.ips[0]
-        tester.run()
-except VmCreationError as exc:
-    resp = exc.api_response
-    print(f"VmCreationError: status={resp.status_code} url={resp.url}", file=sys.stderr)
-    print(f"Response body: {resp.text}", file=sys.stderr)
-    raise
+    try:
+        with EphemeralIpBlock(test_recipe_name + '_ip') as ip_block:
+            tester = ImageTester()
+            tester.server_ip = ip_block.ips[0]
+            tester.run()
+    except VmCreationError as exc:
+        resp = exc.api_response
+        print(f"VmCreationError: status={resp.status_code} url={resp.url}", file=sys.stderr)
+        print(f"Response body: {resp.text}", file=sys.stderr)
+        raise
+
+if __name__ == '__main__':
+    main()
